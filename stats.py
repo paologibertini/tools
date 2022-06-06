c937a5500979032a6802d66168779ddd47454a48
paologibertini <71469104+paologibertini@users.noreply.github.com>
2022-06-06 15:12:08 +0200

    Update stats.py

--- stats.py
+++ stats.py
@@ -3,17 +3,18 @@ import sys
 import argparse
 import statistics
 
-parser = argparse.ArgumentParser(description="Math to int file" )
+parser = argparse.ArgumentParser(description="File to stat" )
 parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), help="Input file (default: stdin)", default=sys.stdin)
 parser.add_argument("-d", dest='sep', help="Fields separator (default: \"|\")", default="|")
 parser.add_argument("-f", dest='field', help="Field pos (default: 0)", default=0)
-parser.add_argument("-op", dest='op', help="Operation type (count, mean, cmean, gmean, stdev, var, cvar, median)", default="mean")
+parser.add_argument("-op", dest='op', help="Operation type (count, mean, cmean, gmean, stdev, var, median)", default="mean")
 args = parser.parse_args()
 
 def ops(name):
     operations = \
         {
             "mean": (1, statistics.mean),
+            "cmean": (2, lambda x: statistics.mean(x) * len(x) / (len(x) - 1))
             "count": (1, len),
             "var": (2, statistics.variance),
             "stdev": (2, statistics.stdev),
